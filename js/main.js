eb572d95953a7be99e23cfc5bd755eb9154b3356
camilogiordano <cgiordano.ontb@gmail.com>
2024-05-26 19:21:21 -0300

    fix html y css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 //       Torneo de penales vs Messi
 
-//       Preguntar nombre del jugador
-//       Elegir si queres atajar o patear
 //       Son 5 penales
-
+//       Elegir si queres atajar o patear
+//       Elegir a donde queres patear/tirarte para atajar
+//       Messi se tira/patea random a uno de esos lugares
+//       10% chances de patear afuera
 //       Penales 
 //       _______________________
 //      |  1                  2 |
@@ -12,162 +13,124 @@
 //      |  3                  4 |
 
 
-//      Elegir a donde queres patear/tirarte para atajar
-//      La maquina se tira random a uno de esos lugares
-//      alert avisandote si metiste/te metieron el gol o no
-//      llevar conteo de goles / cantidad de disparos
-//      10% chances de patear afuera
-//      alert Avisar si ganaste
 
+function pedir_num(texto, minimo, maximo){
+    let num = Number(prompt(texto))
+    while (isNaN(num) || num < minimo || num > maximo){
+        alert("Por favor ingresa un número valido.")
+        num = Number(prompt(texto))
+    }
+    return num
+}
 
+function juego_patear(penales){
+    let jugadorPateando = 0
+    let maquinaAtajando = 0
+    for (i = 0; i < penales ; i++) {
+        let pateaP1 = pedir_num("¿A dónde pateas? \n1: Angulo superior izquierdo. \n2: Angulo superior derecho. \n3: Angulo inferior izquierdo. \n4: Angulo inferior derecho. \n5: Centro", 1, 5)
+        let fallo = (Math.random() < 0.1)
+        let atajaM1 = Math.round(Math.random()*5);
+        alert ("Piiiiiiiiiii, va a patearrrrr ")
+        if (fallo == true){
+            alert("Maliiisimo, pateaste afuera.")
+            fallo++
+        } else if (atajaM1 == pateaP1){
+            alert("Te la atajo el mejor del mundo, Lionel Messi.")
+            maquinaAtajando++
+        } else {
+            alert ("Gooooooooool")
+            jugadorPateando++
+        }
+        alert("Marcador: " +nombre + ": " +jugadorPateando+ "/ " + (i+1))
+    }
+    return jugadorPateando
+}
 
-// let tirarMoneda = prompt("Vamos a tirar la moneda. "+nombre+" ¿Cara o Seca?").toLowerCase
+function juego_atajar(penales){
+    let jugadorAtajando = 0
+    let maquinaPateando = 0
+    for (i = 0; i < penales ; i++) {
+        let atajaP1 = pedir_num("¿A dónde te tiras? \n1: Angulo superior izquierdo. \n2: Angulo superior derecho. \n3: Angulo inferior izquierdo. \n4: Angulo inferior derecho. \n5: Centro", 1, 5)
+        let pateaM1 = Math.round(Math.random()*5);
+        alert ("Piiiiiiiiiii, Mmmmmeeeeeeeeeeeeeessssssiiiiiiiiiiiiiiiiiiii...")
+        if (pateaM1 == atajaP1){
+            alert("Increible pero cierto, le atajaste un penal a Messi.")
+            jugadorAtajando++
+        } else {
+            alert ("Goooooooooooooooooooooooooool del n1 Lionel Andrés Messi")
+            maquinaPateando++
+        }
+        alert("Marcador: Messi: " +maquinaPateando+ "/ " + (i+1))
+    }
+    return maquinaPateando
+}
 
-// if (tirarMoneda === "cara") {
-    
-// }
-
+alert("Torneo de penales")
 let nombre = prompt("Nombre jugador").toLowerCase()
 
-let jugadorPateando = 0
-let jugadorAtajando = 0
-let maquinaPateando = 0
-let maquinaAtajando = 0
-let fallo = 0
-
-let eleccion = Number(prompt("Elegí: (1) Para patear primero, (2) Para atajar primero"))
-
-
-if (eleccion == 1) {
-    alert("Perfecto, " + nombre + " empezas pateando.")
-    alert("Le vas a patear 5 penales seguidos a Messi y despúes Messi te va a patear 5 penales a vos.")
-
-    for (i = 0; i < 5 ; i++) {
-        let pateaP1 = Number(prompt("¿A dónde pateas? 1: Angulo superior izquierdo. 2: Angulo superior derecho. 3: Angulo inferior izquierdo. 4: Angulo superior derecho. 5: Centro"))
-        if (isNaN(pateaP1)){
-            
-        }
-        let fallo = (Math.random() < 0.1)
-        let atajaM1 = Math.round(Math.random()*5);
-        alert ("Piiiiiiiiiii, va a patearrrrr ")
-        if (fallo == true){
-            alert("Maliiisimo, pateaste afuera.")
-            fallo++
-        } else if (atajaM1 == pateaP1){
-            alert("Te la atajo el mejor del mundo, Lionel Messi.")
-            maquinaAtajando++
+while (true) {
+    let jugadorPateando = 0;
+    let maquinaPateando = 0;
+    let fallo = 0
+    let eleccion = pedir_num("Elegí: (1) Para patear primero, (2) Para atajar primero", 1, 2)
+    if (eleccion == 1) {
+        alert("Perfecto, " + nombre + " empezas pateando.")
+        alert("Le vas a patear 5 penales seguidos a Messi y despúes Messi te va a patear 5 penales a vos.")
+        
+        jugadorPateando = juego_patear(5)
+        
+        alert("Fin de la ronda. Metiste: "+jugadorPateando+" goles.")
+        if (jugadorPateando <= 2){
+            alert("Bastante flojo pateando, vamos a ver si sos mejor atajando")
         } else {
-            alert ("Gooooooooool")
-            jugadorPateando++
+            alert("Bueeena, ahora hay que ver si Messi patea mejor que vos")
         }
-        alert("Marcador: " +nombre + ": " +jugadorPateando+ "/ " + (i+1))
-    }
-
-    alert("Fin de la ronda. Metiste: "+jugadorPateando+" goles.")
-    if (jugadorPateando <= 2){
-        alert("Bastante flojo pateando, vamos a ver si sos mejor atajando")
-    } else {
-        alert("Bueeena, ahora hay que ver si Messi patea mejor que vos")
-    }
-    alert("Siguiente ronda: Ahora patea Messi")
-
-    for (i = 0; i < 5 ; i++) {
-        let atajaP1 = Number(prompt("¿A dónde te tiras? 1: Angulo superior izquierdo. 2: Angulo superior derecho. 3: Angulo inferior izquierdo. 4: Angulo superior derecho. 5: Centro"))
-        let pateaM1 = Math.round(Math.random()*5);
-        alert ("Piiiiiiiiiii, Mmmmmeeeeeeeeeeeeeessssssiiiiiiiiiiiiiiiiiiii...")
-        if (pateaM1 == atajaP1){
-            alert("Increible pero cierto, le atajaste un penal a Messi.")
-            jugadorAtajando++
-        } else {
-            alert ("Goooooooooooooooooooooooooool del n1 Lionel Andrés Messi")
-            maquinaPateando++
-        }
-        alert("Marcador: Messi: " +maquinaPateando+ "/ " + (i+1))
-    }
-
-} else if (eleccion == 2) {
-    alert("Biennnn, " + nombre + " empezas atajando.")
-    alert("Messi te va a patear 5 penales seguidos y despúes vos le vas a patear 5 penales a él.")
-    
-    for (i = 0; i < 5 ; i++) {
-        let atajaP1 = Number(prompt("¿A dónde te tiras? 1: Angulo superior izquierdo. 2: Angulo superior derecho. 3: Angulo inferior izquierdo. 4: Angulo superior derecho. 5: Centro"))
-        let pateaM1 = Math.round(Math.random()*5);
-        alert ("Piiiiiiiiiii, Mmmmmeeeeeeeeeeeeeessssssiiiiiiiiiiiiiiiiiiii...")
-        if (pateaM1 == atajaP1){
-            alert("Increible pero cierto, le atajaste un penal a Messi.")
-            jugadorAtajando++
-        } else {
-            alert ("Goooooooooooooooooooooooooool del n1 Lionel Andrés Messi")
-            maquinaPateando++
-        }
-        alert("Marcador: Messi: " +maquinaPateando+ "/ " + (i+1))
-    }
-
-    alert("Fin de la ronda. Messi metio: "+maquinaPateando+" goles.")
-    if (maquinaPateando <= 3){
-        alert("Muuuy raro esto, que le hayas atajado tantos penales a Messi es rarisimo.")
-    } else {
-        alert("Bastaaaante malo atajando, vamos a ver si sabes patear")
-    }
-    alert("Siguiente ronda: Ahora pateas vos.")
-
-    for (i = 0; i < 5 ; i++) {
-        let pateaP1 = Number(prompt("¿A dónde pateas? 1: Angulo superior izquierdo. 2: Angulo superior derecho. 3: Angulo inferior izquierdo. 4: Angulo superior derecho. 5: Centro"))
-        let fallo = (Math.random() < 0.1)
-        let atajaM1 = Math.round(Math.random()*5);
-        alert ("Piiiiiiiiiii, va a patearrrrr ")
-        if (fallo == true){
-            alert("Maliiisimo, pateaste afuera.")
-            fallo++
-        } else if (atajaM1 == pateaP1){
-            alert("Te la atajo el mejor del mundo, Lionel Messi.")
-            maquinaAtajando++
-        } else {
-            alert ("Gooooooooool")
-            jugadorPateando++
-        }
-        alert("Marcador: " +nombre + ": " +jugadorPateando+ "/ " + (i+1))
-    }
-} else {
-    alert("Ingrese 1 o 2. (Patear primero o atajar Primero)")
-}
-
-if(jugadorPateando == maquinaPateando){
-    alert("Empate, vamos a muerte súbita, 1 penal cada uno, empieza " + nombre + " pateando.")
-    while (jugadorPateando == maquinaPateando) {
-        let pateaP1 = Number(prompt("¿A dónde pateas? 1: Angulo superior izquierdo. 2: Angulo superior derecho. 3: Angulo inferior izquierdo. 4: Angulo superior derecho. 5: Centro"))
-        let fallo = (Math.random() < 0.1)
-        let atajaM1 = Math.round(Math.random()*5);
-        alert ("Piiiiiiiiiii, va a patearrrrr ")
-            if (fallo == true){
-                alert("Maliiisimo, pateaste afuera.")
-                fallo++
-            } else if (atajaM1 == pateaP1){
-                alert("Te la atajo el mejor del mundo, Lionel Messi.")
-                maquinaAtajando++
-            } else {
-                alert ("Gooooooooool")
-                jugadorPateando++
-            }
         alert("Siguiente ronda: Ahora patea Messi")
 
-        let atajaP1 = Number(prompt("¿A dónde te tiras? 1: Angulo superior izquierdo. 2: Angulo superior derecho. 3: Angulo inferior izquierdo. 4: Angulo superior derecho. 5: Centro"))
-        let pateaM1 = Math.round(Math.random()*5);
-        alert ("Piiiiiiiiiii, Mmmmmeeeeeeeeeeeeeessssssiiiiiiiiiiiiiiiiiiii...")
-            if (atajaP1 == pateaM1){
-                alert("Increible pero cierto, le atajaste un penal a Messi.")
-                jugadorAtajando++
-            } else {
-                alert ("Goooooooooooooooooooooooooool del n1 Lionel Andrés Messi")
-                maquinaPateando++
-            }
+        maquinaPateando = juego_atajar(5)
+    } else if (eleccion == 2) {
+        alert("Biennnn, " + nombre + " empezas atajando.")
+        alert("Messi te va a patear 5 penales seguidos y despúes vos le vas a patear 5 penales a él.")
+    
+        maquinaPateando = juego_atajar(5)
+
+        alert("Fin de la ronda. Messi metio: "+maquinaPateando+" goles.")
+        if (maquinaPateando <= 3){
+            alert("Muuuy raro todo, que le hayas atajado tantos penales a Messi es rarisimo.")
+        } else {
+            alert("Bastaaaante malo atajando, vamos a ver si sabes patear")
+        }
+            alert("Siguiente ronda: Ahora pateas vos.")
+
+        jugadorPateando = juego_patear(5)
+    } else {
+        alert("Ingrese 1 o 2. (Patear primero o atajar Primero)")
     }
-} 
-if (jugadorPateando > maquinaPateando){
-    alert("Ganaste! Metiste " + jugadorPateando+ " goles. Bien ahí, le ganaste al mismisimo Messi, que solo hizo: " + maquinaPateando+ " goles.")
-} else if (jugadorPateando < maquinaPateando) {
-    alert("Era de esperarse, perdiste. Messi hizo: " + maquinaPateando + " goles. Y vos: " +jugadorPateando +"  ") 
-} else {
-    alert("Camilo arregla este codigo")
+    if (jugadorPateando === maquinaPateando) {
+        alert("Empate, vamos a muerte súbita, 1 penal cada uno, empieza " + nombre + " pateando.");
+
+        while (jugadorPateando === maquinaPateando) {
+            let jugadorGol = juego_patear(1);
+            let maquinaGol = juego_atajar(1);
+            if (jugadorGol > maquinaGol) {
+                jugadorPateando++
+                break;
+            } else if (jugadorGol < maquinaGol) {
+                maquinaPateando++
+                break;
+            }
+        }
+    }
+
+    if (jugadorPateando > maquinaPateando){
+    alert("Ganaste! Bien ahí, le ganaste al mismisimo Messi.")
+    } else if (jugadorPateando < maquinaPateando){
+    alert("Era de esperarse, perdiste. Messi es el mejor del mundo.") 
+    } else {
+        alert("Esto no deberia verse nunca. Por favor comuniquese con servicio técnico.")
+    }
+
+    if (confirm("Quieres salir del programa? \nAceptar para salir \nCancelar para volver a empezar")){
+        break
+    }
 }
-alert("¿Queres jugar de nuevo? Dale f5")
